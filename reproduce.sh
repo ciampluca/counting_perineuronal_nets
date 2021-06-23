2108868f0ec81539034b2799878308bd3a7a987e
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-06-23 18:25:14 +0200

    updated reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -11,7 +11,9 @@ EXPS=(
     perineuronal-nets/detection/fasterrcnn_320
     perineuronal-nets/detection/fasterrcnn_480
     perineuronal-nets/detection/fasterrcnn_640
+    perineuronal-nets/detection/fasterrcnn_640_nococo
     perineuronal-nets/detection/fasterrcnn_800
+    perineuronal-nets/detection/fasterrcnn_800_another_seed
     perineuronal-nets/density/csrnet_256
     perineuronal-nets/density/csrnet_320
     perineuronal-nets/density/csrnet_480
@@ -21,15 +23,20 @@ EXPS=(
 
 for EXP in ${EXPS[@]}; do
     python train.py experiment=$EXP
+    python evaluate.py runs/experiment=$EXP
 done
 
 # VGG CELLS
 EXPS=(
-    vgg-cells/segmentation/unet
-    vgg-cells/detection/fasterrcnn_nms_0.4
-    vgg-cells/density/csrnet
+    vgg-cells/density/csrnet_8_92
+    vgg-cells/density/csrnet_16_84
+    vgg-cells/density/csrnet_32_68
+    vgg-cells/density/csrnet_50_50
+    vgg-cells/density/csrnet_64_36
+    vgg-cells/density/csrnet_80_20
 )
 
 for EXP in ${EXPS[@]}; do
     python train.py experiment=$EXP
+    python evaluate.py runs/experiment=$EXP
 done
