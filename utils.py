384604700ce35f81dba636bd52f3526bf55901b7
ciampi <ciampluca@gmail.com>
2021-11-23 11:50:22 +0100

    - changes for reproducibility

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import collections
 import os
 from pathlib import Path
 import re
+import random
 
 import numpy as np
 import torch
@@ -9,12 +10,20 @@ import torch
 
 def seed_everything(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
     np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = True
-    torch.backends.cudnn.deterministic = True
+    #torch.use_deterministic_algorithms(True)
+
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2 ** 32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 
 class CheckpointManager:
